94a44ff3b170a65cf58518b071d8e286b8ad0833
stoneskin <stoneskin@gmail.com>
2020-08-14 14:09:43 -0400

    change from hi to hello

---
+++ test.py
@@ -0,0 +1,14 @@
+import os
+path = os.getcwd()
+print(path)
+
+
+#import pickle
+from pickle import dump,load
+list=[1,2,3,4,5]
+myFile=open("testData.txt","wb")
+dump(list,myFile)
+print("list dumpped")
+myFile=open("testData.txt","rb")
+list2= load(myFile)
+print(list2)
